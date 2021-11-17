8c1f1c545ba770c12415aae6ce2dbdf223fd3577
Chuck Grindel <chuck@chuckgrindel.com>
2021-11-17 11:42:18 -0700

    Added contains_item_test.

--- tests/arrays_tests/contains_item_test.sh
+++ tests/arrays_tests/contains_item_test.sh
@@ -14,7 +14,20 @@ source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null || \
 assertions_lib="$(rlocation cgrindel_bazel_shlib/lib/assertions.sh)"
 source "${assertions_lib}"
 
+arrays_lib="$(rlocation cgrindel_bazel_shlib/lib/arrays.sh)"
+source "${arrays_lib}"
 
-# DEBUG BEGIN
-fail "STOP"
-# DEBUG END
+# Unsorted array
+array=(z b y c x aa az)
+
+# Confirm that we find an entry deep in the array
+contains_item az "${array[@]}" || fail "Expected 'az' to be contained in array."
+
+# This test will exit from the shortcut
+contains_item m "${array[@]}" && fail "Expected 'm' not to be contained in array."
+
+# This test will pass the shortcut, but will not find the entry
+contains_item a "${array[@]}" && fail "Expected `a` not to be contained in array."
+
+# If we made it this far, we want to be sure to exit success
+exit 0
