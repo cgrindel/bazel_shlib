ba62c035bbf7eb13473b93525fd4972f991455c7
Chuck Grindel <chuck@chuckgrindel.com>
2021-11-18 10:20:50 -0700

    Added contains_item_sorted.

--- tests/arrays_tests/contains_item_sorted_test.sh
+++ tests/arrays_tests/contains_item_sorted_test.sh
@@ -55,5 +55,58 @@ contains_item_sorted "zc" "${array[@]}" && fail "Expected 'zc' not to be found"
 contains_item_sorted "aa" && fail "Expected 'aa' not to be found in empty array"
 
 
+# DEBUG BEGIN
+
+iterations=1000
+# iterations=100
+# set -x
+
+array=(a b c d e f g h i j k l m n o p q r s t u v w x y z)
+
+# set -x
+# contains_item_sorted "z" "${array[@]}"
+# time contains_item_sorted "z" "${array[@]}"
+
+# set -x
+# contains_item "z" "${array[@]}"
+
+
+echo >&2 "*** CHUCK contains_item a" 
+time for (( i = 0; i < $iterations; i++ )); do contains_item "a" "${array[@]}";  done
+
+echo >&2 "*** CHUCK contains_item_a a" 
+time for (( i = 0; i < $iterations; i++ )); do contains_item_a "a" "${array[@]}";  done
+
+echo >&2 "*** CHUCK contains_item_sorted a" 
+time for (( i = 0; i < $iterations; i++ )); do contains_item_sorted "a" "${array[@]}";  done
+
+echo >&2 "*** CHUCK contains_item z" 
+time for (( i = 0; i < ${iterations}; i++ )); do contains_item "z" "${array[@]}";  done
+
+echo >&2 "*** CHUCK contains_item_a z" 
+time for (( i = 0; i < $iterations; i++ )); do contains_item_a "z" "${array[@]}";  done
+
+echo >&2 "*** CHUCK contains_item_sorted z" 
+time for (( i = 0; i < ${iterations}; i++ )); do contains_item_sorted "z" "${array[@]}";  done
+
+echo >&2 "*** CHUCK contains_item_sorted_a z" 
+time for (( i = 0; i < ${iterations}; i++ )); do contains_item_sorted_a "z" "${array[@]}";  done
+
+echo >&2 "*** CHUCK contains_item_sorted_b z" 
+time for (( i = 0; i < ${iterations}; i++ )); do contains_item_sorted_b "z" "${array[@]}";  done
+
+echo >&2 "*** CHUCK contains_item_sorted_c z" 
+time for (( i = 0; i < ${iterations}; i++ )); do contains_item_sorted_c "z" "${array[@]}";  done
+
+echo >&2 "*** CHUCK contains_item_sorted_d z" 
+time for (( i = 0; i < ${iterations}; i++ )); do contains_item_sorted_d "z" "${array[@]}";  done
+
+
+
+
+fail "STOP"
+
+# DEBUG END
+
 # Made it to the end successfully
 exit 0
