fdff4d8f325d7293b03f098abbf2ef48cab93072
Chuck Grindel <chuck@chuckgrindel.com>
2021-11-30 18:22:06 -0700

    Added assert_arg.

--- tests/execute_binary_tests/execute_binary_with_args_test.sh
+++ tests/execute_binary_tests/execute_binary_with_args_test.sh
@@ -16,30 +16,49 @@ source "${assertions_lib}"
 
 my_bin="$(rlocation cgrindel_bazel_shlib/tests/execute_binary_tests/my_bin_with_args.sh)"
 
+# MARK - Functions
+
+assert_arg() {
+  local output="${1}"
+  local index="${2}"
+  local value="${3}"
+  [[ "${output}" =~ "  ${index}: ${value}" ]] || fail "Expected ${index}: ${value}
+  ${output}
+  "
+}
+
 # MARK - Test that embedded arguments are passed along properly.
 
 output=$("${my_bin}")
 [[ "${output}" =~ "Args Count: 6" ]] || fail "Expected args count of 6.
 ${output}
 "
-[[ "${output}" =~ "  1: --first" ]] || fail "Expected --first
-${output}
-"
-[[ "${output}" =~ "  2: first_value" ]] || fail "Expected first_value
-${output}
-"
-[[ "${output}" =~ "  3: --second" ]] || fail "Expected --second
-${output}
-"
-[[ "${output}" =~ "  4: second value has spaces" ]] || fail "Expected second value has spaces
-${output}
-"
-[[ "${output}" =~ "  5: not_a_flag" ]] || fail "Expected not_a_flag
-${output}
-"
-[[ "${output}" =~ "  6: quoted value with spaces" ]] || fail "Expected quoted value with spaces
-${output}
-"
+assert_arg "${output}" 1 "--first"
+assert_arg "${output}" 2 "first_value"
+assert_arg "${output}" 3 "--second"
+assert_arg "${output}" 4 "second value has spaces"
+assert_arg "${output}" 5 "not_a_flag"
+assert_arg "${output}" 6 "quoted value with spaces"
+
+
+# [[ "${output}" =~ "  1: --first" ]] || fail "Expected --first
+# ${output}
+# "
+# [[ "${output}" =~ "  2: first_value" ]] || fail "Expected first_value
+# ${output}
+# "
+# [[ "${output}" =~ "  3: --second" ]] || fail "Expected --second
+# ${output}
+# "
+# [[ "${output}" =~ "  4: second value has spaces" ]] || fail "Expected second value has spaces
+# ${output}
+# "
+# [[ "${output}" =~ "  5: not_a_flag" ]] || fail "Expected not_a_flag
+# ${output}
+# "
+# [[ "${output}" =~ "  6: quoted value with spaces" ]] || fail "Expected quoted value with spaces
+# ${output}
+# "
 
 # MARK - Test that additional arguments are passed along properly
 
@@ -48,6 +67,7 @@ output=$("${my_bin}" additional_arg)
 [[ "${output}" =~ "Args Count: 7" ]] || fail "Expected args count of 7.
 ${output}
 "
-[[ "${output}" =~ "  7: additional_arg" ]] || fail "Expected additional_arg
-${output}
-"
+assert_arg "${output}" 7 "additional_arg"
+# [[ "${output}" =~ "  7: additional_arg" ]] || fail "Expected additional_arg
+# ${output}
+# "
