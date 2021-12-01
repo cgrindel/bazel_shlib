694fbf172dbc930a339dba0be8afd6ec2ea66e82
Chuck Grindel <chuck@chuckgrindel.com>
2021-11-30 18:24:25 -0700

    Clean up.

--- tests/execute_binary_tests/my_bin.sh
+++ tests/execute_binary_tests/my_bin.sh
@@ -7,22 +7,3 @@ echo "Args Count: ${args_count}"
 for (( i = 1; i <= ${#}; i++ )); do
   echo "  ${i}: ${!i}"
 done
-
-
-# args=()
-# while (("$#")); do
-#   case "${1}" in
-#     *)
-#       args+=("${1}")
-#       shift 1
-#       ;;
-#   esac
-# done
-
-# echo "Args Count: ${#args[@]}"
-# if [[ ${#args[@]} > 0 ]]; then
-#   echo "Args: ${args[@]}"
-#   for (( i = 0; i < ${#args[@]}; i++ )); do
-#     echo "  ${i}: ${args[${i}]}"
-#   done
-# fi
